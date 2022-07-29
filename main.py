2c346348e55a2528d3d0ff08ee2abd0b093024b8
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2022-07-29 14:07:27 +0300

    Bot v1

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import time
 import schedule
 import tweepy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def bot():
-    twitter_auth_keys = {
-        "consumer_key": "85vgIXBt6Wie3chd7A8UoMeH7",
-        "consumer_secret": "YqZZoqqWHFTrsBESQpD0sf1WVdN5X748F36hkRHGnhv5lwcvXj",
-        "access_token": "1519627575051669504-IrYi0bKZjvtL7XoE2C8K1iLhZqk4gm",
-        "access_token_secret": "GWAzL1AXOPoHCxR9tgYdhtvQU1P9snPUY3pdL5GAbp7ZZ"
-    }
+    private_key = os.getenv("CONSUMER_KEY")
+    private_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_secret = os.getenv("ACCESS_SECRET")
 
     auth = tweepy.OAuthHandler(
-        twitter_auth_keys['consumer_key'],
-        twitter_auth_keys['consumer_secret']
-    )
+        private_key, private_secret)
+
     auth.set_access_token(
-        twitter_auth_keys['access_token'],
-        twitter_auth_keys['access_token_secret']
-    )
+        access_token, access_secret)
     api = tweepy.API(auth)
 
     # Upload image
@@ -26,13 +25,10 @@ def bot():
     # Post tweet with image
     tweet = "Perstai"
     post_result = api.update_status(status=tweet, media_ids=[media.media_id])
+    print(post_result)
 
 
-def testbot():
-    print(";:D")
-
-
-schedule.every().friday.at("10:48").do(bot)
+schedule.every().friday.at("9:30").do(bot)
 while True:
     schedule.run_pending()
     time.sleep(1)
