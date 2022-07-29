ea697c918948997db905f1bc3520db18bf64c580
Kristjan Rajaniemi <Kristjan.rajaniemi@live.fi>
2022-07-29 14:35:27 +0300

    twitter auth test 1.2

--- main.py
+++ main.py
@@ -6,35 +6,34 @@ from dotenv import load_dotenv
 
 
 def bot():
-    private_key = os.getenv("CONSUMER_KEY")
-    private_secret = os.getenv("CONSUMER_SECRET")
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
-    access_secret = os.getenv("ACCESS_SECRET")
+    access_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    print(private_key, private_secret, access_token, access_secret)
-    # auth = tweepy.OAuthHandler(
-    #     private_key, private_secret)
+    auth = tweepy.OAuthHandler(
+        consumer_key, consumer_secret)
 
-    # auth.set_access_token(
-    #     access_token, access_secret)
-    # api = tweepy.API(auth)
-    # print(auth)
-    # print(api)
+    auth.set_access_token(
+        access_token, access_secret)
+    api = tweepy.API(auth)
+    print(auth)
+    print(api)
 
-    # # Upload image
-    # media = api.media_upload("perstai.jpg")
-    # # Post tweet with image
-    # tweet = "testi Perstai"
-    # post_result = api.update_status(status=tweet, media_ids=[media.media_id])
-    # print(post_result)
+    # Upload image
+    media = api.media_upload("perstai.jpg")
+    # Post tweet with image
+    tweet = "testi Perstai"
+    post_result = api.update_status(status=tweet, media_ids=[media.media_id])
+    print(post_result)
 
 
 load_dotenv()
 
 print(1234)
-schedule.every().friday.at("11:30").do(bot)
-while True:
-    bot()
-    schedule.run_pending()
-    time.sleep(1)
-    print("\nGMT: "+time.strftime("%a, %d %b %Y %I:%M:%S %p %Z", time.gmtime()))
+schedule.every().friday.at("11:38").do(bot)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+#     print("\nGMT: "+time.strftime("%a, %d %b %Y %I:%M:%S %p %Z", time.gmtime()))
+bot()
